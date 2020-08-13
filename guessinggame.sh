425967dbc765668e0a10f46fb0ced5768d8580dc
Sandra Takla <smtakla8110@gmail.com>
2020-08-13 10:26:51 -0400

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,14 +5,12 @@ function guess_filenum {
 	read guess
 	if [[ $guess -eq $filenum ]]
 	then
-		echo "Amazing!!! You finally guessed it!"
-		echo "Hooooooray"
-		echo "  So we have ..."
+		echo "You guessed it!"
+		echo "  There are ..."
 		for f in $(ls)
 		do
-			echo "  - $f and "
+			echo "  - $f in this directoty"
 		done
-		echo "    ... and that was it."
 	else
 		if [[ $guess -gt $filenum ]]
 		then
@@ -24,6 +22,5 @@ function guess_filenum {
 		fi
 	fi
 }
-echo "Welcome to the guessing game!"
-echo "Guess how many files are in the current directory (pretend you don't know) and press Enter :"
+echo "Guess how many files are in the current directory and press Enter :"
 guess_filenum
